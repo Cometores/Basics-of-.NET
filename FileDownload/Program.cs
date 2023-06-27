7eecd35cea2942f370726e105962cabff2a30628
Cometores <anicometores@gmail.com>
2023-06-27 11:56:26 +0200

    Status bar

---
+++ FileDownload/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Threading.Channels;
+using FileDownload;
+
+
+// DrawStatusBarWithoutLoading();
+SimulateDownloading();
+
+
+void DrawStatusBarWithoutLoading()
+{
+    StatusBar statusBar = new StatusBar(7);
+    Task t = Task.Run(() => statusBar.PrintFullBar());
+    
+    System.Threading.Thread.Sleep(700);
+    Console.WriteLine("Hello");
+    
+    t.Wait();
+}
+
+void SimulateDownloading()
+{
+    StatusBar statusBar = new StatusBar(7);
+    Task t1 = Task.Run(() =>
+    {
+        System.Threading.Thread.Sleep(4000);
+        statusBar.Add();
+    });
+    Task t2 = Task.Run(() =>
+    {
+        System.Threading.Thread.Sleep(200);
+        statusBar.Add();
+    });
+    Task t3 = Task.Run(() =>
+    {
+        System.Threading.Thread.Sleep(1500);
+        statusBar.Add();
+    });
+    Task t4 = Task.Run(() =>
+    {
+        System.Threading.Thread.Sleep(800);
+        statusBar.Add();
+    });
+    Task t5 = Task.Run(() =>
+    {
+        System.Threading.Thread.Sleep(5000);
+        statusBar.Add();
+    });
+    Task t6 = Task.Run(() =>
+    {
+        System.Threading.Thread.Sleep(2200);
+        statusBar.Add();
+    });
+    Task t7 = Task.Run(() =>
+    {
+        System.Threading.Thread.Sleep(1000);
+        statusBar.Add();
+    });
+
+    Task.WaitAll(t1, t2, t3, t4, t5, t6, t7);
+    System.Threading.Thread.Sleep(2000);
+    Console.WriteLine("Download finished");
+}
---
+++ FileDownload/StatusBar.cs
@@ -0,0 +1,114 @@
+﻿namespace FileDownload;
+
+public class StatusBar
+{
+    private readonly int _numberOfFiles;
+    private readonly int _topPosition;
+    private int _filesDownloaded;
+
+    private readonly object _lockConsole = new();
+    private readonly object _lockFiles = new();
+    private Task _loadingTask;
+    private CancellationTokenSource _cancellationTokenSource;
+    private CancellationToken _cancellationToken;
+
+    private static readonly string[] LoadingSymbols =
+    {
+        "―",
+        "\\",
+        "|",
+        "/"
+    };
+    private static readonly int LoadingSymbolsLength = LoadingSymbols.Length;
+
+    public StatusBar(int numberOfFiles)
+    {
+        _filesDownloaded = 0;
+        _numberOfFiles = numberOfFiles;
+        _topPosition = Console.CursorTop;
+        Console.WriteLine($"[{new string('-', numberOfFiles)}]");
+
+        _cancellationTokenSource = new();
+        _cancellationToken = _cancellationTokenSource.Token;
+        _loadingTask = new Task(PrintLoading, _cancellationToken);
+        _loadingTask.Start();
+    }
+
+    public void Add()
+    {
+        lock (_lockFiles)
+        {
+            PrintBar(++_filesDownloaded);
+            if(_filesDownloaded == _numberOfFiles)
+                _cancellationTokenSource.Cancel();
+        }
+    }
+
+    [Obsolete]
+    public void PrintFullBar()
+    {
+        for (int i = 0; i < _numberOfFiles; i++)
+        {
+            System.Threading.Thread.Sleep(1000);
+            lock (_lockConsole)
+            {
+                var leftPosBefore = Console.CursorLeft;
+                var ct = Console.CursorTop;
+                var backgroundColor = Console.BackgroundColor;
+
+                Console.SetCursorPosition(i + 1, _topPosition);
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.Write(" ");
+
+                Console.BackgroundColor = backgroundColor;
+                Console.SetCursorPosition(leftPosBefore, ct);
+            }
+        }
+    }
+
+    private void PrintBar(int leftPosition)
+    {
+        lock (_lockConsole)
+        {
+            var leftPositionBefore = Console.CursorLeft;
+            var topPositionBefore = Console.CursorTop;
+            var backgroundBefore = Console.BackgroundColor;
+
+            Console.SetCursorPosition(leftPosition, _topPosition);
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.Write(" ");
+
+            Console.BackgroundColor = backgroundBefore;
+            Console.SetCursorPosition(leftPositionBefore, topPositionBefore);
+        }
+    }
+
+    private void PrintLoading()
+    {
+        int i = 0;
+        while (true)
+        {  
+            System.Threading.Thread.Sleep(200);
+            lock (_lockConsole)
+            {
+                var leftPositionBefore = Console.CursorLeft;
+                var topPositionBefore = Console.CursorTop;
+
+                Console.SetCursorPosition(_numberOfFiles + 2, _topPosition);
+                
+                if (_cancellationToken.IsCancellationRequested)
+                {
+                    Console.Write(" ");
+                    Console.SetCursorPosition(leftPositionBefore, topPositionBefore);
+                    _cancellationToken.ThrowIfCancellationRequested();
+                
+                }
+                Console.Write(LoadingSymbols[i]);
+
+                Console.SetCursorPosition(leftPositionBefore, topPositionBefore);
+            }
+
+            i = (i + 1) % LoadingSymbolsLength;
+        }
+    }
+}
