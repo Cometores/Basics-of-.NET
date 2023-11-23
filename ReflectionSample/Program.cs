dc8d1d41bbb198878dc4d5c8bbe0f3cc83c44b31
cometores <anicometores@gmail.com>
2023-11-23 17:00:06 +0100

    ReflectionSample init

---
+++ ReflectionSample/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Reflection;
+using ReflectionSample;
+
+var currentAssembly = Assembly.GetExecutingAssembly();
+
+var oneTypeFromCurrentAssembly = currentAssembly.GetType("ReflectionSample.Person");
+
+foreach (var constructor in oneTypeFromCurrentAssembly.GetConstructors())
+{
+    Console.WriteLine(constructor);
+}
+
+foreach (var method in oneTypeFromCurrentAssembly.GetMethods(
+             BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic))
+{
+    Console.WriteLine(method);
+}
+
+
+Console.ReadLine();
+
+
+void TypesAndModules()
+{
+    var typesFromCurrentAssembly = currentAssembly.GetTypes();
+
+    // Print all types from current Assembly
+    foreach (var type in typesFromCurrentAssembly)
+    {
+        Console.WriteLine(type.Name);
+    }
+
+    // Get type for Person in current module
+    var oneTypeFromCurrentAssembly = currentAssembly.GetType("ReflectionSample.Person");
+
+    // Get type from external assembly
+    var externalAssembly = Assembly.Load("System.Text.Json");
+    var oneTypeFromExternalAssembly = externalAssembly.GetType("System.Text.Json.JsonProperty");
+
+    // Get module from external assembly
+    var modulesFromExternalAssembly = externalAssembly.GetModules();
+    var oneModuleFromExternalAssembly = externalAssembly.GetModule("System.Text.Json.dll");
+
+    // Get types from module from external assembly
+    var typesFromModuleFromExternalAssembly = oneModuleFromExternalAssembly.GetTypes();
+    var oneTypeFromModuleFromExternalAssembly = oneModuleFromExternalAssembly.GetType("System.Text.Json.JsonProperty");
+}
