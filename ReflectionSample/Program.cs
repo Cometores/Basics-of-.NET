73d9886572a11166630db47bc86fde0130e091b7
cometores <anicometores@gmail.com>
2024-07-20 17:43:09 +0200

    Reflection Sample | Constructor reflection & usage

--- ReflectionSample/Program.cs
+++ ReflectionSample/Program.cs
@@ -2,23 +2,69 @@
 using ReflectionSample;
 
 var currentAssembly = Assembly.GetExecutingAssembly();
-
 var oneTypeFromCurrentAssembly = currentAssembly.GetType("ReflectionSample.Person");
 
-foreach (var constructor in oneTypeFromCurrentAssembly.GetConstructors())
+var personType = typeof(Person);
+var personConstructors = personType.GetConstructors(
+    BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+
+foreach (var personConstructor in personConstructors)
 {
-    Console.WriteLine(constructor);
+    Console.WriteLine(personConstructor);
 }
 
-foreach (var method in oneTypeFromCurrentAssembly.GetMethods(
-             BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic))
-{
-    Console.WriteLine(method);
-}
+var privatePersonConstructor = personType.GetConstructor(
+    BindingFlags.Instance | BindingFlags.NonPublic,
+    null,
+    new[] { typeof(string), typeof(int) },
+    null);
 
+var person1 = personConstructors[0].Invoke(null);
+var person2 = personConstructors[1].Invoke(new object[] { "Andrey" });
+var person3 = personConstructors[2].Invoke(new object[] { "Andrey", 24 });
+
+var person4 = Activator.CreateInstance("ReflectionSample", "ReflectionSample.Person");
+
+var person5 = Activator.CreateInstance("ReflectionSample",
+    "ReflectionSample.Person",
+    true,
+    BindingFlags.Instance | BindingFlags.Public,
+    null,
+    new object[] { "Andrey" },
+    null,
+    null);
+
+var personTypeFromString = Type.GetType("ReflectionSample.Person");
+var person6 = Activator.CreateInstance(personTypeFromString, new object[] { "Andrey" });
+
+var person7 = Activator.CreateInstance("ReflectionSample",
+    "ReflectionSample.Person",
+    true,
+    BindingFlags.Instance | BindingFlags.NonPublic,
+    null,
+    new object[] { "Andrey", 24},
+    null,
+    null);
+
+var assembly = Assembly.GetExecutingAssembly();
+var person8 = assembly.CreateInstance("ReflectionSample.Person");
 
 Console.ReadLine();
 
+
+void MethodsAndConstructors()
+{
+    foreach (var constructor in oneTypeFromCurrentAssembly.GetConstructors())
+    {
+        Console.WriteLine(constructor);
+    }
+
+    foreach (var method in oneTypeFromCurrentAssembly.GetMethods(
+                 BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic))
+    {
+        Console.WriteLine(method);
+    }
+}
 
 void TypesAndModules()
 {
