b0c8e5074ebf607a0ed37c8cdac353bf830fde2d
cometores <anicometores@gmail.com>
2024-11-12 23:55:51 +0100

    Files | Renamer | Collision bug fixed

--- Rename/Renamer.cs
+++ Rename/Renamer.cs
@@ -22,18 +22,26 @@ static class Renamer
     private static void RenameItems(string path, IFormatter formatter, bool recursive, 
         Func<string, string[]> getItems, Action<string, string> moveItem)
     {
-        foreach (var item in getItems(path))
+        foreach (var oldPath in getItems(path))
         {
-            string newName = formatter.Format(Path.GetFileName(item));
-            string newPath = Path.Combine(Path.GetDirectoryName(item), newName);
-            
-            if (item != newPath)
-                moveItem(item, newPath);
+            string directory = Path.GetDirectoryName(oldPath)!;
+            string extension = Path.GetExtension(oldPath);
+            string originalName = Path.GetFileNameWithoutExtension(oldPath);
+            string formattedName = formatter.Format(originalName);
+            string newPath = Path.Combine(directory, formattedName + extension);
+        
+            int suffix = 1;
+            while (File.Exists(newPath) || Directory.Exists(newPath))
+            {
+                newPath = Path.Combine(directory, $"{formattedName}_{suffix}{extension}");
+                suffix++;
+            }
+
+            if (oldPath != newPath)
+                moveItem(oldPath, newPath);
 
             if (recursive && Directory.Exists(newPath))
-            {
                 RenameFilesAndFolders(newPath, formatter, recursive);
-            }
         }
     }
 }
